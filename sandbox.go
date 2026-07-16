299b2551472fbf167b71446907433b830d9c06ec
marshone <marshone@users.noreply.github.co>
2026-07-16 09:30:14 -0400

    fix: rename sanbox.go to sandbox.go

--- sandbox.go
+++ sandbox.go
@@ -9,9 +9,9 @@ type AlignedStruct struct {
 
 // BrokenStruct is intentionally misaligned to test our diagnostic output.
 type BrokenStruct struct {
-	IsActive   bool     // 1 byte   (Offset: 0)
-	DataBuffer []byte   // 24 bytes (Offset: 24)
-	Flag       byte     // 1 byte   (Offset: 48)
-	Name       string   // 16 bytes (Offset: 56)
-	CallbackID uint64   // 8 bytes  (Offset: 72)
+	IsActive   bool   // 1 byte   (Offset: 0)
+	DataBuffer []byte // 24 bytes (Offset: 24)
+	Flag       byte   // 1 byte   (Offset: 48)
+	Name       string // 16 bytes (Offset: 56)
+	CallbackID uint64 // 8 bytes  (Offset: 72)
 }
